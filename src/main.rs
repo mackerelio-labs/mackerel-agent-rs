69304c97f2c1e07d9077f1355f48de33eaf06a04
Krout0n <krouton@hatena.ne.jp>
2020-07-17 15:35:20 +0900

    依存ライブラリ突っ込んでconfファイル読むようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{load_yaml, App};
+use ini::Ini;
+use std::{fs::File, io::prelude::*, path::Path};
+
+fn main() -> std::io::Result<()> {
+    let yaml = load_yaml!("../cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+    let path = Path::new(
+        matches
+            .value_of("config")
+            .unwrap_or("/usr/local/etc/mackerel-agent.conf"),
+    );
+    let conf = Ini::load_from_file(path).unwrap();
+    for (key, val) in &conf {
+        dbg!((key, val));
+    }
+    Ok(())
 }
