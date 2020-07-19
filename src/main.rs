c240834eaccdc0e113b49b9fd78ddc243f937176
Krout0n <krouton@hatena.ne.jp>
2020-07-19 21:41:36 +0900

    ちょっとだけConfig読むようになった

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
 use clap::{load_yaml, App};
 use ini::Ini;
-use std::{fs::File, io::prelude::*, path::Path};
+use std::{collections::HashMap, fs::File, io::prelude::*, path::Path};
+
+#[derive(Debug)]
+struct Config {
+    api_key: String,
+    apibase: String,
+}
+
+impl Config {
+    fn new() -> Self {
+        Self {
+            api_key: String::new(),
+            apibase: String::new(),
+        }
+    }
+
+    fn from_ini(ini: ini::Ini) -> Self {
+        let mut conf = Self::new();
+        let map = &ini
+            .iter()
+            .map(|(_, val)| val.iter().collect::<HashMap<_, _>>())
+            .collect::<Vec<_>>()[0];
+        conf.api_key = map.get("apikey").unwrap().to_string();
+        conf.apibase = map.get("apibase").unwrap().to_string();
+        conf
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let yaml = load_yaml!("../cli.yml");
@@ -10,9 +36,7 @@ fn main() -> std::io::Result<()> {
             .value_of("config")
             .unwrap_or("/usr/local/etc/mackerel-agent.conf"),
     );
-    let conf = Ini::load_from_file(path).unwrap();
-    for (key, val) in &conf {
-        dbg!((key, val));
-    }
+    let ini = Ini::load_from_file(path).unwrap();
+    dbg!(Config::from_ini(ini));
     Ok(())
 }
